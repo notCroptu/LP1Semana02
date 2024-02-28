8b9d5b301a7ba357d719106866f093641eda1bc0
notCroptu <martinsmariana1972@gmail.com>
2024-02-28 18:53:58 +0000

    inputs Ex 5

---
+++ Cilindro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere a altura do cilindro: ");
+            float altura = float.Parse(Console.ReadLine());
+
+            Console.Write("Insere o raio do cilindro: ");
+            float raio = float.Parse(Console.ReadLine());
+        }
+    }
+}
