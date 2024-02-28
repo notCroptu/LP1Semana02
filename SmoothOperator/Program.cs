b4277429280bc0b7c6417bc735320d1ccd1b1d50
notCroptu <martinsmariana1972@gmail.com>
2024-02-28 11:38:29 +0000

    Smooth Operator Ex1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro não-negativo:");
+            byte n = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{--n}");
+            Console.WriteLine($"{++n}");
+        }
+    }
+}
