ba5950a181bccc988fc6d5470c8a9d8d92c9e39b
notCroptu <martinsmariana1972@gmail.com>
2024-02-28 19:20:27 +0000

    Added Volume stuff Ex 5

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,10 +7,12 @@ namespace Cilindro
         static void Main(string[] args)
         {
             Console.Write("Insere a altura do cilindro: ");
-            float altura = float.Parse(Console.ReadLine());
+            float a = float.Parse(Console.ReadLine());
 
             Console.Write("Insere o raio do cilindro: ");
-            float raio = float.Parse(Console.ReadLine());
+            float r = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Volume do cilindro: { MathF.PI * MathF.Pow(r, 2) * a}");
         }
     }
 }
